9dc7d29f6fc3b5046f5ce4b99bb6f79db01a2b03
Marco Marchetta <marchetta@fesrv.e4.cluster>
2025-02-24 11:45:10 +0100

    added 1/2c factor

--- src/mol_properties/ao_magn_dip_becke.irp.f
+++ src/mol_properties/ao_magn_dip_becke.irp.f
@@ -3,7 +3,8 @@
 &BEGIN_PROVIDER [ double precision, ao_magdip_z_becke, (ao_num,ao_num)]
  implicit none
  integer :: i,j,ipoint
- double precision :: r(3), weight
+ double precision :: r(3), weight, k 
+ k=1.d0/(2.d0*137.035999177)
  ao_magdip_x_becke=0.d0
  ao_magdip_y_becke=0.d0
  ao_magdip_z_becke=0.d0
@@ -14,11 +15,11 @@
     r(2) = final_grid_points(2,ipoint)
     r(3) = final_grid_points(3,ipoint)
     weight = final_weight_at_r_vector(ipoint)
-    ao_magdip_x_becke(j,i) +=  weight * ((aos_in_r_array_transp(ipoint,j)*r(2)*aos_grad_in_r_array_transp_bis(ipoint,i,3))&
+    ao_magdip_x_becke(j,i) += k* weight * ((aos_in_r_array_transp(ipoint,j)*r(2)*aos_grad_in_r_array_transp_bis(ipoint,i,3))&
                                           -(aos_in_r_array_transp(ipoint,j)*r(3)*aos_grad_in_r_array_transp_bis(ipoint,i,2 )))
-    ao_magdip_y_becke(j,i) +=  weight * ((aos_in_r_array_transp(ipoint,j)*r(3)*aos_grad_in_r_array_transp_bis(ipoint,i,1))&
+    ao_magdip_y_becke(j,i) += k* weight * ((aos_in_r_array_transp(ipoint,j)*r(3)*aos_grad_in_r_array_transp_bis(ipoint,i,1))&
                                           -(aos_in_r_array_transp(ipoint,j)*r(1)*aos_grad_in_r_array_transp_bis(ipoint,i,3 )))
-    ao_magdip_z_becke(j,i) +=  weight * ((aos_in_r_array_transp(ipoint,j)*r(1)*aos_grad_in_r_array_transp_bis(ipoint,i,2))&
+    ao_magdip_z_becke(j,i) += k* weight * ((aos_in_r_array_transp(ipoint,j)*r(1)*aos_grad_in_r_array_transp_bis(ipoint,i,2))&
                                           -(aos_in_r_array_transp(ipoint,j)*r(2)*aos_grad_in_r_array_transp_bis(ipoint,i,1 ))) 
    enddo
   enddo
